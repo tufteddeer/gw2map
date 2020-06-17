e30634d3108e60b07262b31afacf7778b68fb018
tufteddeer <tufteddeer@disroot.org>
2020-06-17 20:57:00 +0200

    display map names

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 "use strict";
 
-let map
+let leafletmap
 
 document.addEventListener("DOMContentLoaded", () => {
     initMap()
@@ -8,15 +8,55 @@ document.addEventListener("DOMContentLoaded", () => {
 
 function initMap() {
 
-    map = L.map('map').setView([0, 0], 4);
+    leafletmap = L.map('map').setView([0, 0], 4);
 
     L.tileLayer('https://tiles.guildwars2.com/{continent_id}/{floor}/{z}/{x}/{y}.jpg', {
-        attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
         continent_id: 1,
         floor: 1,
         minZoom: 0,
         maxZoom: 7,
         noWrap: true
-    }).addTo(map);
+    }).addTo(leafletmap);
 
+    console.log("fetching api data...")
+
+    const endpoint = "https://api.guildwars2.com/v1/map_floor.json?continent_id=1&floor=1" // TODO: use real endpoint, TODO: v2?
+    fetch("/apiresult.json")
+        .then(response => response.json())
+        .then(data => {
+            console.log("loaded data")
+            renderData(data)
+        })
+        .catch((error => {
+            console.error(error)
+    }))
+}
+
+function renderData(data) {
+    // todo: validate input
+
+    const regions = data.regions
+
+    for (let r in regions) {
+        for (let m in regions[r].maps) {
+            const map = regions[r].maps[m]
+
+            //filter out the many story maps
+            if (map.tasks.length === 0 &&
+                map.skill_challenges.length === 0) {
+                continue
+            }
+
+            const labelCoords = unproject(leafletmap, map.label_coord)
+
+            const marker = new L.marker(labelCoords, {opacity: 0.0});
+            marker.bindTooltip(map.name, {permanent: true, className: "region-label", offset: [0, 0]});
+            marker.addTo(leafletmap);
+        }
+    }
+}
+
+// convert points to map positions (lat/lng) that can be displayed on a map
+function unproject(map, coordinates) {
+    return map.unproject(coordinates, map.getMaxZoom())
 }
