fe894c47efed9088e80e1e09b9e8ff0d7d45f324
tufteddeer <tufteddeer@disroot.org>
2020-07-05 13:10:07 +0200

    use real endpoint

--- map.js
+++ map.js
@@ -69,7 +69,7 @@ class Gw2Map {
         console.log("fetching api data...")
 
         const endpoint = "https://api.guildwars2.com/v1/map_floor.json?continent_id=1&floor=1"
-        fetch("/apiresult.json")
+        fetch(endpoint)
             .then(response => response.json())
             .then(data => {
                 console.log("loaded data")
